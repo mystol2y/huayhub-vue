7b083f4c3cf487bce3ffb6487e35d0b892d25d99
mystol2y <wasanthon@gmail.com>
2021-04-22 13:52:45 +0700

    add static configuration

--- src/views/Signin.vue
+++ src/views/Signin.vue
@@ -1,62 +1,76 @@
 <template>
   <div class="Signin">
-    <Navbar/>
+    <Navbar />
     <div class="container card m-card">
-       <div class="logologin">
-      <img src="../assets/img/huay999logonew.png" alt="">
+      <div class="logologin">
+        <img src="../assets/img/huay999logonew.png" alt="" />
       </div>
-    <form class="p-form">
-  <div class="form-group row text-left">
-    <label for="inputEmail3" class="col-lg-12 col-md-12 col-sm-12 col-form-label">Email</label>
-    <div class="col-lg-12 col-md-12 col-sm-12">
-      <input type="email" class="form-control" id="inputEmail3">
+      <form class="p-form">
+        <div class="form-group row text-left">
+          <label
+            for="inputEmail3"
+            class="col-lg-12 col-md-12 col-sm-12 col-form-label"
+            >Email</label
+          >
+          <div class="col-lg-12 col-md-12 col-sm-12">
+            <input type="email" class="form-control" id="inputEmail3" />
+          </div>
+        </div>
+        <div class="form-group row mt-3 text-left">
+          <label
+            for="inputPassword3"
+            class="col-lg-12 col-md-12 col-sm-12 col-form-label"
+            >Password</label
+          >
+          <div class="col-lg-12 col-md-12 col-sm-12">
+            <input type="password" class="form-control" id="inputPassword3" />
+          </div>
+        </div>
+        <fieldset class="form-group row mt-3">
+          <legend class="col-form-label col-sm-12 float-sm-left pt-0"></legend>
+          <div class="form-group row">
+            <div class="col-lg-12 col-md-12 col-sm-12 pl-input">
+              <div class="form-check">
+                <input
+                  class="form-check-input"
+                  type="checkbox"
+                  id="gridCheck1"
+                />
+                <label class="form-check-label" for="gridCheck1">
+                  จดจำรหัสผ่าน
+                </label>
+              </div>
+            </div>
+          </div>
+        </fieldset>
+        <div class="form-group row">
+          <div class="col-sm-12">
+            <router-link class="btn btn-primary" to="/indexmember" tag="button"
+              >เข้าสู่ระบบ</router-link
+            >
+          </div>
+        </div>
+      </form>
     </div>
-  </div>
-  <div class="form-group row mt-3 text-left">
-    <label for="inputPassword3" class="col-lg-12 col-md-12 col-sm-12 col-form-label">Password</label>
-    <div class="col-lg-12 col-md-12 col-sm-12">
-      <input type="password" class="form-control" id="inputPassword3">
-    </div>
-  </div>
-  <fieldset class="form-group row mt-3">
-    <legend class="col-form-label col-sm-12 float-sm-left pt-0"></legend>
-    <div class="form-group row">
-    <div class="col-lg-12 col-md-12 col-sm-12 pl-input">
-      <div class="form-check">
-        <input class="form-check-input" type="checkbox" id="gridCheck1">
-        <label class="form-check-label" for="gridCheck1">
-          จดจำรหัสผ่าน
-        </label>
-      </div>
-    </div>
-  </div>
-  </fieldset>
-  <div class="form-group row">
-    <div class="col-sm-12">
-      <router-link class="btn btn-primary" to="/indexmember" tag="button">เข้าสู่ระบบ</router-link>
-    </div>
-  </div>
-</form>
-</div>
   </div>
 </template>
 <script>
-import Navbar from '@/components/Navbar.vue'
+import Navbar from "@/components/Navbar.vue";
 export default {
-  name: 'Signin',
-  data(){
+  name: "Signin",
+  data() {
     return {
-      status: true
-    }
+      status: true,
+    };
   },
   components: {
     Navbar,
     // News
-  }
-}
+  },
+};
 </script>
 <style scoped>
- /* .Signin {
+/* .Signin {
    min-height: 94vh;
  }
  .m-card {
@@ -90,68 +104,89 @@ export default {
 } */
 .card {
   margin: 50px auto;
-  background-image: repeating-linear-gradient(358deg, rgb(0,0,0) 0px, rgb(0,0,0) 27px,transparent 27px, transparent 29px),repeating-linear-gradient(280deg, rgb(0,0,0) 0px, rgb(0,0,0) 27px,transparent 27px, transparent 29px),repeating-linear-gradient(337deg, rgb(0,0,0) 0px, rgb(0,0,0) 27px,transparent 27px, transparent 29px),linear-gradient(90deg, rgb(225, 16, 162),rgb(169, 105, 71));
+  background-image: repeating-linear-gradient(
+      358deg,
+      rgb(0, 0, 0) 0px,
+      rgb(0, 0, 0) 27px,
+      transparent 27px,
+      transparent 29px
+    ),
+    repeating-linear-gradient(
+      280deg,
+      rgb(0, 0, 0) 0px,
+      rgb(0, 0, 0) 27px,
+      transparent 27px,
+      transparent 29px
+    ),
+    repeating-linear-gradient(
+      337deg,
+      rgb(0, 0, 0) 0px,
+      rgb(0, 0, 0) 27px,
+      transparent 27px,
+      transparent 29px
+    ),
+    linear-gradient(90deg, rgb(225, 16, 162), rgb(169, 105, 71));
   color: white;
 }
-.p-form{
-   padding: 30px 100px;
- }
-  .Signin{
+.p-form {
+  padding: 30px 100px;
+}
+.Signin {
   height: 93vh;
-  }
- .logologin {
-   margin: 100px auto;
- }
- .logologin img{
-   width: 250px;
- }
- @media (min-width: 1024px) and (max-width: 1366px) {
-    .Signin{
-  height: 95vh;
-  }
-  .card {
-  margin: 200px auto;
 }
 .logologin {
-   margin: 50px auto;
- }
- .pl-input {
-   margin-left: 20px;
- }
+  margin: 100px auto;
+}
+.logologin img {
+  width: 250px;
+}
+@media (min-width: 1024px) and (max-width: 1366px) {
+  .Signin {
+    height: 95vh;
+  }
+  .card {
+    margin: 200px auto;
+  }
+  .logologin {
+    margin: 50px auto;
+  }
+  .pl-input {
+    margin-left: 20px;
+  }
 }
 @media (min-width: 768px) and (max-width: 1023px) {
   .card {
-  margin: 200px auto;
-}
-.logologin {
-   margin: 50px auto;
- }
- .pl-input {
-   margin-left: 20px;
- }
+    margin: 200px auto;
+  }
+  .logologin {
+    margin: 50px auto;
+  }
+  .pl-input {
+    margin-left: 20px;
+  }
 }
 @media (min-width: 250px) and (max-width: 767px) {
-  .p-form{
-   padding: 0;
- }
-  .Signin{
-  height: auto;
+  .p-form {
+    padding: 0;
+  }
+  .Signin {
+    height: auto;
   }
   .card {
-  margin: 140px auto;
-}
-.logologin {
-   margin: 20px auto;
- }
-.logologin img{
-   width: 150px;
-   margin-bottom: 20px;
- }
- /* .row {
+    margin: 140px auto;
+  }
+  .logologin {
+    margin: 20px auto;
+  }
+  .logologin img {
+    width: 150px;
+    margin-bottom: 20px;
+  }
+  /* .row {
    margin: 0;
  } */
- .pl-input {
-   margin-left: 20px;
- }
+  .pl-input {
+    margin-left: 20px;
+  }
 }
 </style>
--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -5,32 +5,58 @@
       <div class="logologup">
         <img src="../assets/img/huay999logonew.png" alt="" />
       </div>
+      <h4 class="text-center">สมัครสมาชิก</h4>
       <form class="p-form">
-        <h4 class="text-center">สมัครสมาชิก</h4>
+        <h4 class="text-danger regis">
+          <span class="badge badge-pill badge-warning text-center">STEP 1</span>
+          ระบุเบอร์โทร
+        </h4>
         <div class="form-group row text-left">
           <label
             for="inputfistname"
             class="col-lg-12 col-md-12 col-sm-12 col-form-label"
             >ลงทะเบียนเบอร์โทรศัพท์</label
           >
-          <div class="col-lg-12 col-md-12 col-sm-12">
-            <input type="email" class="form-control" id="inputfistname" />
+          <div
+            class="col-4 col-sm-4 col-md-2  d-flex align-items-center justify-content-md-end"
+          >
+            <span class="flag-icon flag-icon-th mr-1"></span> +66
           </div>
+          <div class="col-4 col-lg-8 col-md-8 col-sm-8">
+            <input
+              type="email"
+              class="form-control"
+              placeholder="ใส่เบอร์โทรศัพท์"
+              id="inputfistname"
+            />
+          </div>
+        </div>
+        <div
+          class="col-12 col-sm-12 col-md-6 text-right text-sm-right text-md-left"
+        >
+          <small class="text-secondary"
+            >กรุณาใส่หมายเลขโทรศัพท์เพื่อยืนยันตัวตน</small
+          >
         </div>
         <div class="form-group row text-left mt-3">
           <label
             for="inputlastname"
             class="col-lg-12 col-md-12 col-sm-12 col-form-label"
-            >กรุณากรอกอักษร 4 ตัวในภาพ</label
+            >ยืนยันตัวตน</label
           >
-          <div class="col-lg-12 col-md-12 col-sm-12">
-            <input type="email" class="form-control" id="inputlastname" />
-          </div>
+        </div>
+        <div class="col-sm-10">
+          <vue-recaptcha
+            ref="recaptcha"
+            @verify="onVerify"
+            sitekey="6Le46LEaAAAAAHxTsPbUCL9nXZexytDmFIA06XZC"
+          >
+          </vue-recaptcha>
         </div>
         <div class="form-group row mt-5">
           <div class="col-sm-12">
             <router-link class="btn btn-primary" to="/signup" tag="button"
-              >สมัครสมาชิก</router-link
+              >ถัดไป</router-link
             >
           </div>
         </div>
@@ -40,12 +66,51 @@
 </template>
 
 <script>
+// var axios = require("axios");
 import Navbar from "@/components/Navbar.vue";
+import VueRecaptcha from "vue-recaptcha";
 export default {
   name: "Signup",
+  data() {
+    return {
+      captcha: "",
+      robot: false,
+    };
+  },
   components: {
     Navbar,
+    VueRecaptcha,
     // News
+  },
+  created() {
+    this.getCaptcha();
+  },
+  methods: {
+    submit: function() {
+      if (this.robot) {
+        alert("norobot");
+      }
+    },
+    onVerify: function(response) {
+      if (response) this.robot = true;
+    },
+    // async getCaptcha() {
+    //   var config = {
+    //     method: "get",
+    //     url: "http://localhost:8000/api/register/",
+    //     headers: {
+    //       Authorization:
+    //         "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ3YXNhbiIsImlhdCI6MTYxODkxMjEwNjc3MH0.iD5ml2Bp_k15B1H86616O0M6lcyETj3qLA5ckjcf4yw",
+    //     },
+    //   };
+    //   axios(config)
+    //     .then(function(response) {
+    //       console.log(JSON.stringify(response.data));
+    //     })
+    //     .catch(function(error) {
+    //       console.log(error);
+    //     });
+    // },
   },
 };
 </script>
